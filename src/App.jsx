e4b715bb596319bedf2386f112ad7f91d1599e9f
alextalijan <acatalijan@icloud.com>
2025-08-28 19:42:22 +0200

    feat: structure app into multiple sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import GeneralSection from './components/GeneralSection.jsx';
+import EducationalSection from './components/EducationalSection.jsx';
+import ExperienceSection from './components/ExperienceSection.jsx';
+
 function App() {
   return (
-    <></>
+    <>
+      <GeneralSection />
+      <EducationalSection />
+      <ExperienceSection />
+    </>
   )
 }
 
