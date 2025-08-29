b188c39737bb3b3f221f24cf7759f97e39b1ac97
alextalijan <acatalijan@icloud.com>
2025-08-29 16:45:13 +0200

    feat: add functionality to delete jobs from the list

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -4,12 +4,16 @@ import { useState } from 'react';
 function ExperienceSection() {
   const [jobs, setJobs] = useState([]);
 
+  function deleteJob(jobId) {
+    setJobs(jobs.filter(job => job.id !== jobId));
+  }
+
   return (
     <div className='jobsList'>
      <h2 className='section-heading'>Experience</h2>
      {jobs.map((job) => {
       return (
-        <Job key={job.id} companyName={job.companyName} position={job.position} responsibilities={job.responsibilities} fromDate={job.fromDate}  toDate={job.toDate} />
+        <Job key={job.id} companyName={job.companyName} position={job.position} responsibilities={job.responsibilities} fromDate={job.fromDate}  toDate={job.toDate} handleDelete={() => deleteJob(job.id)} />
       )
      })}
      <button className='add-job-btn' onClick={() => setJobs([...jobs, { id: crypto.randomUUID(), companyName: '', position: '', responsibilities: '', fromDate: '', toDate: '' }])}>Add Job</button>
